7d541518f2dffd29278be77b314d83b1eba477dd
Evan Becker <me@evanbecker.com>
2021-03-16 23:46:54 -0500

    temporarily ignore https

--- MuskyMulisha/Startup.cs
+++ MuskyMulisha/Startup.cs
@@ -42,7 +42,7 @@ namespace MuskyMulisha
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthorization();
